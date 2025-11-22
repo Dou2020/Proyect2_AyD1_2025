a65e9293efe1242dc9526d305edd9e2e7866f39a
Dou <doueduar2018@gmail.com>
2025-11-22 12:52:57 -0600

    add proyect build routing

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/commerce/branch/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/subcursales/:id/comercios',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/subcursales/:id/tarifas',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/commerce/branch/*',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
